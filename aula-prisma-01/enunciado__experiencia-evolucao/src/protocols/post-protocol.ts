30af16911e675e8a48137632231055a734c0939d
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-09-20 17:28:51 -0300

    Aula 1 exercício

--- aula-prisma-01/enunciado__experiencia-evolucao/src/protocols/post-protocol.ts
+++ aula-prisma-01/enunciado__experiencia-evolucao/src/protocols/post-protocol.ts
@@ -2,7 +2,8 @@ export type Post = {
   id: number;
   username: string;
   title: string;
-  body: string;
+  content: string;
+  createdAt?: Date;
 }
 
 export type ApplicationError = {
--- aula-prisma-01/enunciado__experiencia-evolucao/src/schemas/post-schema.ts
+++ aula-prisma-01/enunciado__experiencia-evolucao/src/schemas/post-schema.ts
@@ -4,5 +4,6 @@ import { CreatePost } from "../repositories/post-repository";
 export const postSchema = Joi.object<CreatePost>({
   username: Joi.string().required(),
   title: Joi.string().required(),
-  body: Joi.string().required()
+  content: Joi.string().required(),
+  createdAt: Joi.date()
 })
